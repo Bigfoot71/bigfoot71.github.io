0e71292687dbd6dc78c51e669f29960520f9ea6e
Bigfoot71 <victor.lejuez@protonmail.ch>
2026-07-29 12:51:05 +0200

    mdx support for all pages

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const projects = defineCollection({
-	loader: glob({ pattern: '**/*.md', base: './src/content/projects' }),
+	loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/projects' }),
 	schema: z.object({
 		title: z.string(),
 		image: z.string().optional(),
@@ -12,7 +12,7 @@ const projects = defineCollection({
 });
 
 const blog = defineCollection({
-	loader: glob({ pattern: '**/*.md', base: './src/content/blog' }),
+	loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/blog' }),
 	schema: z.object({
 		title: z.string(),
 		image: z.string().optional(),
